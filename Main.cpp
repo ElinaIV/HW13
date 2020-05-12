3c5b327189aa7dd325bea9838a57e09a9e569328
Elina <49618499+ElinaIV@users.noreply.github.com>
2020-05-12 23:23:43 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,11 @@
 #include <random>
 #include <vector>
 #include <SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
 
-auto width = 800U;
-auto height = 600U;
+const auto width = 800U;
+const auto height = 600U;
 
 class Molecula {
 public:
@@ -12,17 +14,17 @@ public:
 	float r;
 
 	Molecula() {
-		dx = 10;
-		dy = 10;
+		dx = gen_from_to(0, 15);
+		dy = gen_from_to(0, 15);
 		r = 3.0f;
 		x = gen_from_to(10, width - 10);
 		y = gen_from_to(10, height - 10);
-		std::cout << "(" << x << " ; " << y << ")" << std::endl;
 	}
 
 	void draw(sf::RenderWindow* window) {
 		sf::CircleShape circle(2.0f * r);
 		circle.setPosition(x, y);
+
 		circle.setFillColor(sf::Color::Red);
 
 		window->draw(circle);
@@ -37,20 +39,22 @@ public:
 	}
 
 	void move() {
-		x += dx;
-		y += dy;
+		if (x > width) x = 0; if (x < 0) x = width;
+		if (y > height) y = 0; if (y < 0) y = height;
+		x += gen_from_to(-10, 10);
+		y += gen_from_to(-10, 10);
 	}
 };
 
 int main() {
-
-	std::vector<Molecula> moleculs;
-	Molecula m;
-	Molecula mm;
-	moleculs.push_back(m);
-	moleculs.push_back(mm);
+	std::vector<Molecula*> moleculs;
+	int count = 15;
+	for (int i = 0; i < count; i++) {
+		moleculs.push_back(new Molecula());
+	}
 
 	sf::RenderWindow window{ sf::VideoMode(width, height), "BROWN" };
+	window.setFramerateLimit(90);
 	while (window.isOpen()) {
 		sf::Event event;
 
@@ -60,11 +64,11 @@ int main() {
 			}
 		}
 
-		window.clear();
-
 		for (auto m : moleculs) {
-			m.draw(&window);
-			m.move();
+			m->move();
+			m->draw(&window);
+			window.display();
+			window.clear();
 		}
 
 		window.display();
